d4a917182f3f1c7e4561767ff7c02573b558404f
Fabricio Xavier <fabricio.chagas@bhs.com.br>
2019-05-24 16:25:13 -0300

    Criação da camada de dados.

--- src/Eventos.IO/Eventos.IO.Domain/Organizadores/Organizador.cs
+++ src/Eventos.IO/Eventos.IO.Domain/Organizadores/Organizador.cs
@@ -1,18 +1,35 @@
 ﻿using Eventos.IO.Domain.Core.Models;
+using Eventos.IO.Domain.Eventos;
 using System;
+using System.Collections.Generic;
 
 namespace Eventos.IO.Domain.Organizadores
 {
     public class Organizador : Entity<Organizador>
     {
-        public Organizador(Guid id)
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+
+        public Organizador(Guid id, string nome, string cpf, string email)
         {
             Id = id;
+            Nome = nome;
+            CPF = cpf;
+            Email = email;
         }
+
+        // EF contrutor
+
+        public Organizador() { }
+
+        //EF propriedade de navegação
+
+        public virtual ICollection<Evento> Eventos { get; set; }
 
         public override bool EhValido()
         {
-            throw new System.NotImplementedException();
+            return true;
         }
     }
 }
